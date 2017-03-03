098ff0259ae583ee7a9479a3b62b479c7338ef01
Criticalstone <kevinhoog@gmail.com>
2017-03-03 15:00:09 +0100

    Adds path visualization and makes model rotate around back pos

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -2,9 +2,12 @@
 import rospy
 import math
 import tf
+import os
 
-from visualization_msgs.msg import Marker
-from geometry_msgs.msg import Pose
+from visualization_msgs.msg import Marker, MarkerArray
+from geometry_msgs.msg import Pose, PoseStamped
+from geometry_msgs.msg import Point
+from nav_msgs.msg import Path
 
 class Visualizer:
     def __init__(self):
@@ -12,6 +15,7 @@ class Visualizer:
         rospy.Subscriber('sim_pose', Pose, self.poseCallback)
         self.pub = rospy.Publisher('truck_marker', Marker, queue_size=10)
         self.truck = TruckModel()
+        self.path = TruckPath()
         rospy.spin()
 
     def poseCallback(self, msg):
@@ -19,7 +23,43 @@ class Visualizer:
         markers = self.truck.getMarkers()
         self.pub.publish(markers[0])
         self.pub.publish(markers[1])
+        self.path.pub.publish(self.path.path)
         
+class TruckPath:
+    def __init__(self):
+        self.points = [(0,0,0)]
+        self.pub = rospy.Publisher('sim_path', Path, queue_size=10)
+        self.createPath('/../path.txt')
+
+        self.path = Path()
+        self.path.header.frame_id = 'truck'
+        self.path.header.stamp = rospy.Time.now()
+        id = 0
+        for m in self.points:
+            posestmpd = PoseStamped()
+
+            posestmpd.header.seq = id
+            posestmpd.header.frame_id = 'truck'
+            posestmpd.header.stamp = rospy.Time.now()
+
+            posestmpd.pose.position.x = m[0]
+            posestmpd.pose.position.y = m[1]
+            posestmpd.pose.position.z = m[2]
+
+            posestmpd.pose.orientation.w = 0.0
+
+            self.path.poses.append(posestmpd)
+            id += 1
+        
+        print self.path
+    
+    def createPath(self, path):
+        dirpath = os.path.dirname(os.path.abspath(__file__))
+        f = open(dirpath+path, 'r')
+        lines = [line.rstrip('\n') for line in f.readlines()]
+        posL = [s.split(' ', 1 ) for s in lines]
+        for l in posL:
+            self.points.append((int(l[0]),int(l[1]),0))
 
 class TruckModel:
     def __init__(self):
@@ -39,7 +79,7 @@ class TruckModel:
         self.header.lifetime = rospy.Duration(0)
         
         # Truck measurments in mm
-        self.header.scale.x = 270 #420
+        self.header.scale.x = 420 #420
         self.header.scale.y = 190
         self.header.scale.z = 220
 
@@ -66,7 +106,7 @@ class TruckModel:
         self.trailer.lifetime = rospy.Duration(0)
         
         # Truck measurments in mm
-        self.trailer.scale.x = 490
+        self.trailer.scale.x = 420
         self.trailer.scale.y = 190
         self.trailer.scale.z = 220
 
@@ -85,6 +125,9 @@ class TruckModel:
         return [self.header, self.trailer]
 
     def setPose(self, pose):
+        theta = tf.transformations.euler_from_quaternion((pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w))[2]
+        pose.position.x = pose.position.x + (self.header.scale.x/2) * math.cos(theta)
+        pose.position.y = pose.position.y + (self.header.scale.x/2) * math.sin(theta)
         self.header.pose = pose
 
     def setPosition(self, position):
